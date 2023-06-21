85df2eb90c9449a87e00c6eb69c9fb3bd36d81b7
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-06-21 10:56:08 +0700

    kondisi if untuk menampilkan isi dari filedan kondisi jika file tidak ditemukan

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -38,7 +38,17 @@ int main() {
 
 	cout << endl << ">= Membuka dan Membaca file " << endl;
 	//jika file ada maka
-	
+	if (infile.is_open()) {
+		// melakukan perulangan setiap baris
+		while (getline(infile, baris)) {
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	// jika tidak menemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
 	return 0;
 }
 
