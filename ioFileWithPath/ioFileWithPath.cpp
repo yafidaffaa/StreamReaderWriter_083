6251be58712da703871bc099cd86c857fd656004
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-06-21 11:05:35 +0700

    loop untuk memasukan isi file yang berhenti jika memasukan karakter 'q'

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -19,7 +19,23 @@ int main() {
 
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
-	
+	//unlimited loop untuk menulis
+	while (true)
+	{
+		cout << "- ";
+
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
+
+
 }
 
 
