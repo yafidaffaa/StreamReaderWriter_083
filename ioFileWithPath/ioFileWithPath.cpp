370b1b881ec6966bad44331f17cf4838c93b54fe
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-06-21 11:07:39 +0700

    membuat file dengan mode membaca

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -35,6 +35,11 @@ int main() {
 	//selesai dalam menulis sekarang tutup filenya
 	outfile.close();
 
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open(NamaFile, ios::in);
+
 
 }
 
