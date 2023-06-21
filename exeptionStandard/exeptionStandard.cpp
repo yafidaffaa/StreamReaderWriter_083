ec2d0fe5fabb54aec3b3e0446af2d063001eacf5
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-06-21 11:32:50 +0700

    membuat kondisi kemudian yang diambil atau ditampilkan

--- exeptionStandard/exeptionStandard.cpp
+++ exeptionStandard/exeptionStandard.cpp
@@ -7,7 +7,13 @@ using namespace std;
 
 int main() {
 
-
+	cout << "Awal program" << endl; //penanda 1
+	try {
+		array <int, 3> data = { 3,5,7 };
+		//pesan array int 3 element
+		cout << data.at(5) << endl;
+		//memanggil array index ke 5
+	}
 
 
 	return 0;
