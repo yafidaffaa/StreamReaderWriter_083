bd994d3e8c1b39ababa2dde2c964712d7f903549
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-06-21 11:23:43 +0700

    jika memasukan sebuah int maka akan di eksekusi

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -4,8 +4,11 @@ using namespace std;
 int main() {
 	try {
 		cout << "Selamat Belajar di Prodi TI UMY" << endl;
-
 		throw 0.5; //melempar sebuah integer maka
 		cout << "Pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a) {
+		//block ini akan di eksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
 	}
 }
