7492946b5fc4db5de7e7ad80d741345afd79f69e
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-06-21 11:00:28 +0700

    file dengan mode menulis dan menujukan nama file yang ingin di masukan

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -9,7 +9,17 @@ int main() {
 	string baris;
 	string NamaFile;
 
+	cout << "Masukan Nama FIle : ";
+	cin >> NamaFile;
 
+	//membuka file dengan mode menulis
+	ofstream outfile;
+	//menunjuk ke subuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	
 }
 
 
