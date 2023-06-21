4bb5f68de479930b3102768957e353a0f741d57f
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-06-21 10:53:38 +0700

    kondisi while untuk memasukan ke variable baris

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -15,7 +15,18 @@ int main() {
 
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
+	//unlimited loop untuk menulis
+	while (true)
+	{
+		cout << "- ";
 
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 }
 
 
